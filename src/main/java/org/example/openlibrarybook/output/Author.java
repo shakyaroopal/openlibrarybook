80cd3f9b2d544ff5731136d59e3a5d276ed6e715
Roopal Shakya <shakyaroopal121@gmail.com>
2025-01-22 14:59:33 +0530

    added author

---
+++ src/main/java/org/example/openlibrarybook/output/Author.java
@@ -0,0 +1,13 @@
+package org.example.openlibrarybook.output;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class Author {
+    private String authorId;
+    private String authorName;
+}
